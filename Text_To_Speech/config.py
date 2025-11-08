eac6370d2987f7b12096301316441c5eff42d0ac
Avijit Bhadra <codewithavijit@gmail.com>
2025-11-08 15:12:34 +0530

    add

--- Text_To_Speech/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    API_KEY = "mysecretapikey123"  # Change to your secure key
-    OUTPUT_DIR = "generated_audio"
--- Text_To_Speech/modules/utils.py
+++
@@ -1,5 +0,0 @@
-import re
-
-def safe_filename(text):
-    """Create a filesystem-safe filename from text"""
-    return re.sub(r'[^a-zA-Z0-9_-]', '_', text)
